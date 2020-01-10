4075fd710b2573ce2ef54279f63c034c41bd7769
Verena Blaschke <verena.blaschke@gmx.de>
2020-01-10 14:34:12 +0100

    first commit

---
+++ src/main/java/de/tuebingen/sfs/submodulefromroot/SubmoduleFromRoot.java
@@ -0,0 +1,7 @@
+package de.tuebingen.sfs.submodulefromroot;
+
+public class SubmoduleFromRoot {
+	public static void greet() {
+		System.out.println("Running the submodule with full folder structure");
+	}
+}
