f578eb86e5385f0a04202299323247df7bb9d5b0
Verena Blaschke <verena.blaschke@gmx.de>
2020-01-10 14:43:44 +0100

    Update the submodule

--- src/main/java/de/tuebingen/sfs/submodulefromroot/SubmoduleFromRoot.java
+++ src/main/java/de/tuebingen/sfs/submodulefromroot/SubmoduleFromRoot.java
@@ -1,6 +1,14 @@
 package de.tuebingen.sfs.submodulefromroot;
 
+import de.tuebingen.sfs.submoduledemo.App;
+
 public class SubmoduleFromRoot {
+
+	public static void main(String args[]) {
+		greet();
+		App.greet();
+	}
+
 	public static void greet() {
 		System.out.println("Running the submodule with full folder structure");
 	}
